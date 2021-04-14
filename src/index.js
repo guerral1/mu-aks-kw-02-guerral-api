0497bfb84a8abd81f8b796bf6bc96a352a8943d1
Yamen Samman <yamen_samman@manulife.ca>
2021-04-14 14:13:25 -0400

    Keep CORS open by default then have students change it after ingress exercise

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ if (
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
